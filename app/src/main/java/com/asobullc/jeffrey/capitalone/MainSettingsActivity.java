bc9ec83221c64bd6eb00c7e6484f525d53b3998b
Juh-Jiang <JenniferEJiang@gmail.com>
2014-11-08 21:50:29 -0600

    Main Settings View
    very basic view

---
+++ app/src/main/java/com/asobullc/jeffrey/capitalone/MainSettingsActivity.java
@@ -0,0 +1,12 @@
+package com.asobullc.jeffrey.capitalone;
+
+import android.widget.Button;
+
+/**
+ * Created by MacProJJ on 11/8/14.
+ */
+public class MainSettingsActivity {
+    Button addAccount;
+
+
+}
