677cd3e8a93885cea867731436c8a57d26ba2005
kevinszwagiel <szwagielkevin@gmail.com>
2014-11-08 19:57:35 -0600

    MainJava

--- app/src/main/java/com/asobullc/jeffrey/capitalone/MainActivity.java
+++ app/src/main/java/com/asobullc/jeffrey/capitalone/MainActivity.java
@@ -4,14 +4,38 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
 
 public class MainActivity extends Activity {
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                try {
+                    if(validateLogin() == true)
+                        Toast.makeText(getBaseContext(), "Logged Successful", Toast.LENGTH_LONG).show();
+                    else
+                        Toast.makeText(getBaseContext(), "Login Failed", Toast.LENGTH_LONG).show();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
 
@@ -35,5 +59,20 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public boolean validateLogin() throws IOException {
+        String host = "jet.codebasics.com";
+        int port = 1566;
+
+        Socket socket = new Socket(host, port);
+
+        PrintWriter serverOut = new PrintWriter(socket.getOutputStream(), true);
+
+        BufferedReader serverIn = new BufferedReader(
+                new InputStreamReader(socket.getInputStream()));
+
+        serverOut.write("v " + ((EditText)findViewById(R.id.username)).getText().toString() +
+                " " + ((EditText)findViewById(R.id.password)).getText().toString());
     }
 }
